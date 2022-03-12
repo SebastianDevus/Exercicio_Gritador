7bff88064ece1f5b414a9567b5d0a0e3d7b71839
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-12 18:32:33 -0300

    Atividade realizada

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string textoOriginal;
+
+Console.Write("Por favor escreva algo: ");
+textoOriginal = Console.ReadLine()!; // Lê o que o usuário escreveu e armazina na string textoOriginal
+
+Console.WriteLine(textoOriginal.ToUpper()); // Exibe o texto novamente, em MAIÚSCULO
